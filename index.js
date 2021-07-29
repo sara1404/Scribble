544701032af579c708f25c930ca0154c0bc5493a
sara1404 <sara14042000@gmail.com>
2021-07-29 23:11:07 +0200

    Added view engine

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const path = require('path');
 const PORT = 3000;
 
+app.set('view engine', 'pug');
+app.set('views', './public/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile('skribl.html', { root:path.join(__dirname, '/public/html/') });
 });
+
+app.get('/ljubav', (req, res) => {
+    res.render('index.pug');
+})
 
 
 
