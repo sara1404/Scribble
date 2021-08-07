8eeeadc2e3eb581181611cccd48e4d4e17683a13
sara1404 <sara14042000@gmail.com>
2021-08-07 20:51:13 +0200

    private room finished

--- public/scripts/clipboard.js
+++ public/scripts/clipboard.js
@@ -1,11 +1,17 @@
 
-function copyToClipboard() {
-    var text = document.getElementById('linksobe').innerHTML;
-    console.log(text);
+function copyToClipboard(linkElementId) {
+    console.log(linkElementId);
+    var text = document.getElementById(linkElementId).innerHTML;
     var input = document.body.appendChild(document.createElement("input"));
-    input.value = text;
+    input.value = document.URL + '/' + text;
+    console.log(input.value);
     input.focus();
     input.select();
     document.execCommand('copy');
     input.parentNode.removeChild(input);
+    
+    //show snackbar
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 }
